2beb1bc47238bbc06578a06f4b4f70b0d6377940
Carlos Henrique <carlosh.mb@outlook.com>
2023-06-22 22:41:05 -0300

    chore: add nunito font family and expo fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import Box from "./src/components/common/Box";
 
+import {
+  useFonts,
+  Nunito_400Regular,
+  Nunito_700Bold,
+} from "@expo-google-fonts/nunito";
+
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_700Bold,
+  });
+
+  if (!fontsLoaded) return <ActivityIndicator size={"small"} />;
+
   return (
     <View style={styles.container}>
       <Box>
-        <Text>Open up App.tsx to start working on your app!</Text>
+        <Text style={{ fontFamily: "Nunito_700Bold" }}>
+          Open up App.tsx to start working on your app!
+        </Text>
       </Box>
       <StatusBar style="auto" />
     </View>

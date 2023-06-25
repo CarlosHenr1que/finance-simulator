4a10a473bce72bcffb0230129d2ed1ae83b12719
Carlos Henrique <carlosh.mb@outlook.com>
2023-06-25 20:56:07 -0300

    feat: add stack component

---
+++ src/components/common/Stack/index.tsx
@@ -0,0 +1,25 @@
+import React, { Children } from "react";
+
+import Box from "../Box";
+import { Props as BoxProps } from "../Box";
+
+interface StackProps extends BoxProps {
+  children: React.ReactNode;
+  spacing: number;
+}
+
+const Stack: React.FC<StackProps> = ({ children, spacing, ...rest }) => {
+  const arrayChildren = Children.toArray(children);
+
+  const isLastItem = (index: number) => arrayChildren.length === index + 1;
+
+  return (
+    <Box dir="column" {...rest}>
+      {arrayChildren.map((child, index) => (
+        <Box mb={isLastItem(index) ? 0 : spacing}>{child}</Box>
+      ))}
+    </Box>
+  );
+};
+
+export default Stack;

a095b7ac9713d0d0f572cb7b76910d9e8f40549a
Carlos Henrique <carlosh.mb@outlook.com>
2023-06-26 09:27:29 -0300

    fix: layout for web

--- src/components/common/Container/index.tsx
+++ src/components/common/Container/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 import * as S from "./styles";
+import Box from "../Box";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Container: React.FC<Props> = ({ children }) => {
-  return <S.ContentWrapper>{children}</S.ContentWrapper>;
+  return (
+    <Box width="100%" justify="center" background="#fbfbfb">
+      <S.ContentWrapper>{children}</S.ContentWrapper>
+    </Box>
+  );
 };
 
 export default Container;

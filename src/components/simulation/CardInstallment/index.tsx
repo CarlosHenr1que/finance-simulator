98d2ad1029ff165b51e07ac85b72fdb03c7b93f6
Carlos Henrique <carlosh.mb@outlook.com>
2023-06-26 09:27:40 -0300

    feat: add card installment

---
+++ src/components/simulation/CardInstallment/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Text from "../../common/Text";
+import Box from "../../common/Box";
+
+interface CardInstallmentProps {
+  order: string;
+  price: string;
+  debit: string;
+}
+const CardInstallment: React.FC<CardInstallmentProps> = ({
+  order,
+  price,
+  debit,
+}) => {
+  return (
+    <Box dir="column" width="100%" background="#fff" py={8} px={8} radius={8}>
+      <Box width="100%" justify="space-between">
+        <Text size={14} color="secondary">
+          Parcela
+        </Text>
+        <Text size={14} color="secondary">
+          Valor
+        </Text>
+        <Text size={14} color="secondary">
+          Saldo devedor
+        </Text>
+      </Box>
+      <Box mt={8} width="100%" justify="space-between">
+        <Text size={14} weight="bold">
+          {order}
+        </Text>
+        <Text size={14} weight="bold">
+          {price}
+        </Text>
+        <Text size={14} weight="bold">
+          {debit}
+        </Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default CardInstallment;

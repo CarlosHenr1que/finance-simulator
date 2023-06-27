cee6f94c63d2d2b3f82c80aef85e1794ba6c31cc
Carlos Henrique <carlosh.mb@outlook.com>
2023-06-26 22:03:41 -0300

    feat: add fade animation to card information

--- src/components/simulation/CardInformation/index.tsx
+++ src/components/simulation/CardInformation/index.tsx
@@ -3,6 +3,12 @@ import React from "react";
 import Text from "../../common/Text";
 import Box from "../../common/Box";
 
+import ReAnimated, {
+  FadeInLeft,
+  FadeOutLeft,
+  Layout,
+} from "react-native-reanimated";
+
 interface CardInformationProps {
   icon: any;
   title: string;
@@ -16,33 +22,40 @@ export const CardInformation = ({
   description,
   iconBackground,
 }: CardInformationProps) => (
-  <Box
-    dir="row"
-    background="#fff"
-    width="100%"
-    py={8}
-    px={8}
-    radius={8}
-    align="center"
+  <ReAnimated.View
+    style={{ width: "100%" }}
+    layout={Layout}
+    entering={FadeInLeft.duration(1000)}
+    exiting={FadeOutLeft.duration(100)}
   >
     <Box
-      width={30}
-      height={30}
-      background={iconBackground}
-      radius={30}
-      mr={8}
-      justify="center"
+      dir="row"
+      background="#fff"
+      width="100%"
+      py={8}
+      px={8}
+      radius={8}
       align="center"
     >
-      {icon}
+      <Box
+        width={30}
+        height={30}
+        background={iconBackground}
+        radius={30}
+        mr={8}
+        justify="center"
+        align="center"
+      >
+        {icon}
+      </Box>
+      <Box dir="column">
+        <Text weight="bold" size={18}>
+          {title}
+        </Text>
+        <Text color="secondary" size={14}>
+          {description}
+        </Text>
+      </Box>
     </Box>
-    <Box dir="column">
-      <Text weight="bold" size={18}>
-        {title}
-      </Text>
-      <Text color="secondary" size={14}>
-        {description}
-      </Text>
-    </Box>
-  </Box>
+  </ReAnimated.View>
 );

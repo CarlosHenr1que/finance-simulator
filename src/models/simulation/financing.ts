e4ec45462d28ec8785487efe9c68cd78cf2c7b03
Carlos Henrique <carlosh.mb@outlook.com>
2023-06-27 22:50:29 -0300

    feat: add valuation and down payment props

--- src/models/simulation/financing.ts
+++ src/models/simulation/financing.ts
@@ -3,10 +3,12 @@ export interface Installment {
   fee: number;
   installment: number;
   debit: number;
+  valuation?: number;
 }
 
 export interface Simulation {
   financing: number;
+  downPayment?: number;
   fee: number;
   installmentsNumber: number;
   installments: Installment[];

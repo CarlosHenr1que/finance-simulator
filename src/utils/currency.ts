e6724a620a990db1deb09faf2d4269ec4bc1f326
Carlos Henrique <carlosh.mb@outlook.com>
2023-06-27 22:53:27 -0300

    refactor: format with thousand separation

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -2,7 +2,16 @@ export const formatCurrency = (value: number | undefined) => {
   if (!value) {
     return;
   }
-  return `R$ ${Number(String(value).replace(",", "."))
-    .toFixed(2)
-    .replace(".", ",")}`;
+  const priceStr: string = value.toFixed(2);
+  const [integerPart, decimalPart] = priceStr.split(".");
+  let formattedInteger = "";
+  for (let i = integerPart.length - 1, count = 0; i >= 0; i--, count++) {
+    formattedInteger = integerPart[i] + formattedInteger;
+    if (count % 3 === 2 && i !== 0) {
+      formattedInteger = "." + formattedInteger;
+    }
+  }
+  const formattedPrice = `R$ ${formattedInteger},${decimalPart}`;
+
+  return formattedPrice;
 };

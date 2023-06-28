b93a6ceac1080aad0890378d7aec3ef7d198ab61
Carlos Henrique <carlosh.mb@outlook.com>
2023-06-28 12:02:28 -0300

    feat: add items to card information

--- src/components/simulation/CardInformation/index.tsx
+++ src/components/simulation/CardInformation/index.tsx
@@ -9,11 +9,16 @@ import ReAnimated, {
   Layout,
 } from "react-native-reanimated";
 
+interface Item {
+  title: string;
+  description: string;
+}
 interface CardInformationProps {
   icon: any;
   title: string;
   description: string;
   iconBackground: string;
+  items?: Item[];
 }
 
 export const CardInformation = ({
@@ -21,6 +26,7 @@ export const CardInformation = ({
   title,
   description,
   iconBackground,
+  items,
 }: CardInformationProps) => (
   <ReAnimated.View
     style={{ width: "100%" }}
@@ -28,33 +34,39 @@ export const CardInformation = ({
     entering={FadeInLeft.duration(1000)}
     exiting={FadeOutLeft.duration(100)}
   >
-    <Box
-      dir="row"
-      background="#fff"
-      width="100%"
-      py={8}
-      px={8}
-      radius={8}
-      align="center"
-    >
-      <Box
-        width={30}
-        height={30}
-        background={iconBackground}
-        radius={30}
-        mr={8}
-        justify="center"
-        align="center"
-      >
-        {icon}
+    <Box dir="column" background="#fff" width="100%" py={8} px={8} radius={8}>
+      <Box dir="row" align="center">
+        <Box
+          width={30}
+          height={30}
+          background={iconBackground}
+          radius={30}
+          mr={8}
+          justify="center"
+          align="center"
+        >
+          {icon}
+        </Box>
+        <Box dir="column">
+          <Text weight="bold" size={18}>
+            {title}
+          </Text>
+          <Text color="secondary" size={14}>
+            {description}
+          </Text>
+        </Box>
       </Box>
-      <Box dir="column">
-        <Text weight="bold" size={18}>
-          {title}
-        </Text>
-        <Text color="secondary" size={14}>
-          {description}
-        </Text>
+      <Box justify="space-between" align="center" mt={10} px={8}>
+        {items?.map((item) => (
+          <Box dir="column">
+            <Text weight="bold" size={16}>
+              {item.title}
+            </Text>
+            <Text color="secondary" size={14}>
+              {item.description}
+            </Text>
+          </Box>
+        ))}
       </Box>
     </Box>
   </ReAnimated.View>

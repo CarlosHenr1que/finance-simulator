572dc2ccd9635df145b6028a5d604058a287135a
Carlos Henrique <carlosh.mb@outlook.com>
2023-07-16 10:55:18 -0300

    refactor: memorize card information

--- src/components/simulation/CardInstallment/index.tsx
+++ src/components/simulation/CardInstallment/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 
 import Text from "../../common/Text";
 import Box from "../../common/Box";
@@ -8,44 +8,42 @@ interface CardInstallmentProps {
   price: string;
   debit: string;
 }
-const CardInstallment: React.FC<CardInstallmentProps> = ({
-  order,
-  price,
-  debit,
-}) => {
-  return (
-    <Box
-      dir="column"
-      width="100%"
-      background="secondary"
-      py={8}
-      px={8}
-      radius={8}
-    >
-      <Box width="100%" justify="space-between">
-        <Text size={14} color="secondary">
-          Parcela
-        </Text>
-        <Text size={14} color="secondary">
-          Valor
-        </Text>
-        <Text size={14} color="secondary">
-          Saldo devedor
-        </Text>
+const CardInstallment: React.FC<CardInstallmentProps> = memo(
+  ({ order, price, debit }) => {
+    return (
+      <Box
+        dir="column"
+        width="100%"
+        background="secondary"
+        py={8}
+        px={8}
+        radius={8}
+      >
+        <Box width="100%" justify="space-between">
+          <Text size={14} color="secondary">
+            Parcela
+          </Text>
+          <Text size={14} color="secondary">
+            Valor
+          </Text>
+          <Text size={14} color="secondary">
+            Saldo devedor
+          </Text>
+        </Box>
+        <Box mt={8} width="100%" justify="space-between">
+          <Text color="primary" size={14} weight="bold">
+            {order}
+          </Text>
+          <Text color="primary" size={14} weight="bold">
+            {price}
+          </Text>
+          <Text color="primary" size={14} weight="bold">
+            {debit}
+          </Text>
+        </Box>
       </Box>
-      <Box mt={8} width="100%" justify="space-between">
-        <Text color="primary" size={14} weight="bold">
-          {order}
-        </Text>
-        <Text color="primary" size={14} weight="bold">
-          {price}
-        </Text>
-        <Text color="primary" size={14} weight="bold">
-          {debit}
-        </Text>
-      </Box>
-    </Box>
-  );
-};
+    );
+  }
+);
 
 export default CardInstallment;

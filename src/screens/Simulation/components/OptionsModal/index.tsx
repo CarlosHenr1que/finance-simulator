e596b9f593c5098c1ff8b62b760f9e78a5ce9222
Carlos Henrique <carlosh.mb@outlook.com>
2023-07-16 10:56:04 -0300

    feat: add options modal

---
+++ src/screens/Simulation/components/OptionsModal/index.tsx
@@ -0,0 +1,84 @@
+import React, { ReactNode, useState } from "react";
+import Modal from "../../../../components/common/Modal";
+import CardButton from "../../../../components/simulation/CardButton";
+import Text from "../../../../components/common/Text";
+import Box from "../../../../components/common/Box";
+import Icon from "@expo/vector-icons/MaterialIcons";
+import { TouchableOpacity } from "react-native";
+
+import ReAnimated, {
+  FadeInRight,
+  FadeOutLeft,
+  Layout,
+} from "react-native-reanimated";
+
+interface Option {
+  title: string;
+  description: string;
+  content: ReactNode;
+}
+
+interface OptionsModalProps {
+  visible: boolean;
+  title: string;
+  options: Option[];
+  onClose: () => void;
+}
+
+const OptionsModal: React.FC<OptionsModalProps> = ({
+  visible,
+  title,
+  options,
+  onClose,
+}) => {
+  const [selectedOption, setSelectedOption] = useState<number>(-1);
+
+  const onCloseWrapper = () => {
+    onClose();
+    setSelectedOption(-1);
+  };
+
+  const currentOption = options[selectedOption];
+
+  return (
+    <Modal visible={visible} onClose={onCloseWrapper}>
+      {selectedOption !== -1 ? (
+        <TouchableOpacity
+          onPress={() => {
+            setSelectedOption(-1);
+          }}
+        >
+          <Box width="100%" align="center" mb={20}>
+            <Icon size={22} color="#000" name="keyboard-arrow-left" />
+            <Text size={24} weight="bold">
+              {currentOption.title}
+            </Text>
+          </Box>
+        </TouchableOpacity>
+      ) : (
+        <ReAnimated.View
+          layout={Layout}
+          entering={FadeInRight.duration(400)}
+          exiting={FadeOutLeft.duration(100)}
+        >
+          <Box mb={20}>
+            <Text size={24} weight="bold">
+              {title}
+            </Text>
+          </Box>
+        </ReAnimated.View>
+      )}
+      {selectedOption !== -1
+        ? options[selectedOption].content
+        : options.map((item, index) => (
+            <CardButton
+              title={item.title}
+              description={item.description}
+              onPress={() => setSelectedOption(index)}
+            />
+          ))}
+    </Modal>
+  );
+};
+
+export default OptionsModal;

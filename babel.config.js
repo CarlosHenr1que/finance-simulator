4c26ed0d275182123898ef36e49834686060eeea
Carlos Henrique <carlosh.mb@outlook.com>
2023-07-24 22:47:05 -0300

    feat: add module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,27 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin', "@babel/plugin-transform-export-namespace-from"],
+    plugins: ['react-native-reanimated/plugin', "@babel/plugin-transform-export-namespace-from", [
+      "module-resolver",
+      {
+        root: ".",
+        extensions: [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+          ".android.js",
+          ".android.tsx",
+          ".ios.js",
+          ".ios.tsx",
+        ],
+        alias: {
+          "@screens": "./src/screens",
+          "@components": "./src/components",
+          "@hooks": "./src/hooks",
+          "@utils": "./src/utils",
+        },
+      },
+    ],]
   };
 };

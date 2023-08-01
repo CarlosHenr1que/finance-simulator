ba3b3a0dab0a144e9a2014495e2d906a3717851b
Carlos Henrique <carlosh.mb@outlook.com>
2023-07-31 23:01:04 -0300

    feat: add options filter to financing simulation

--- src/screens/Financing/screens/Simulation/index.tsx
+++ src/screens/Financing/screens/Simulation/index.tsx
@@ -9,7 +9,7 @@ import { CardInformation } from "@components/simulation/CardInformation";
 import Icon from "@expo/vector-icons/MaterialIcons";
 import MCIcon from "@expo/vector-icons/MaterialCommunityIcons";
 import Stack from "@components/common/Stack";
-import { ScrollView } from "react-native";
+import { FlatList, ScrollView } from "react-native";
 import CardInstallment from "@components/simulation/CardInstallment";
 
 import { formatCurrency } from "@utils/currency";
@@ -22,6 +22,8 @@ import { AmortizationForm as AmortizationFormValues } from "./components/Amortiz
 import { calculateFinance } from "@utils/financing";
 import { useFinancingSimulation } from "@contexts/financing/simulation";
 import SimulationForm from "./components/SimulationForm";
+
+const options = [{ title: "Informações" }, { title: "Parcelas" }];
 
 const Simulation: React.FC = () => {
   const {
@@ -38,6 +40,12 @@ const Simulation: React.FC = () => {
     downPayment,
     valuationPercentage,
   } = simulation;
+
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
+  const onSelectedOption = (option: (typeof options)[0]) => {
+    setSelectedOption(option);
+  };
 
   const getFinancingValue = () => {
     if (downPayment) return formatCurrency(financing - downPayment);
@@ -69,119 +77,142 @@ const Simulation: React.FC = () => {
 
   return (
     <Container>
-      <ScrollView>
-        <OptionsModal
-          visible={isOptionsModalVisible}
-          title="Opções"
-          options={[
-            {
-              title: "Alterar Informações",
-              description: "Altere as informações do seu financiamento",
-              content: <SimulationForm />,
-            },
-            {
-              title: "Amortização",
-              description: "Realizar amortização no seu financiamento",
-              content: <AmortizationForm onSuccess={handleAmortization} />,
-            },
-          ]}
-          onClose={closeOptionModal}
-        />
+      <OptionsModal
+        visible={isOptionsModalVisible}
+        title="Opções"
+        options={[
+          {
+            title: "Alterar Informações",
+            description: "Altere as informações do seu financiamento",
+            content: <SimulationForm />,
+          },
+          {
+            title: "Amortização",
+            description: "Realizar amortização no seu financiamento",
+            content: <AmortizationForm onSuccess={handleAmortization} />,
+          },
+        ]}
+        onClose={closeOptionModal}
+      />
 
-        <Box dir="column" pt={60}>
-          <Box dir="column" px={16}>
-            <Box width="100%" justify="space-between">
-              <Box dir="column">
-                <Text color="primary" size={24} weight="bold">
-                  {getFinancingValue()}
-                </Text>
-                <Text color="secondary" size={14}>
-                  Valor financiado
-                </Text>
-              </Box>
-              <Button
-                width={80}
-                text="Opções"
-                onPress={() => setIsOptionsModalVisible(true)}
-              />
+      <Box flex={1} dir="column" pt={60}>
+        <Box flex={1} dir="column" px={16}>
+          <Box width="100%" justify="space-between">
+            <Box dir="column">
+              <Text color="primary" size={24} weight="bold">
+                {getFinancingValue()}
+              </Text>
+              <Text color="secondary" size={14}>
+                Valor financiado
+              </Text>
             </Box>
+            <Button
+              width={80}
+              text="Opções"
+              onPress={() => setIsOptionsModalVisible(true)}
+            />
+          </Box>
 
-            <Stack spacing={10} mt={20}>
-              <CardInformation
-                icon={<Icon name="payments" color="#fff" size={22} />}
-                title={String(installmentsNumber)}
-                description="Prestações"
-                iconBackground="#000"
-                items={getInstallmentItems(installments)}
-              />
-              <CardInformation
-                icon={<MCIcon name="percent" color="#fff" size={22} />}
-                title={String(fee).replace(".", ",") + "%"}
-                description="Taxa de juros anual"
-                iconBackground="#FF3642"
-                items={[
-                  {
-                    title: "Total juros",
-                    description: formatCurrency(
-                      installments.at(-1)?.totalFee
-                    ) as string,
-                  },
-                  {
-                    title: "Total pago",
-                    description: formatCurrency(
-                      installments.at(-1)?.installmentAmount
-                    ) as string,
-                  },
-                ]}
-              />
-              {downPayment && (
+          <Box mt={20}>
+            <FlatList
+              horizontal
+              ItemSeparatorComponent={() => <Box width={10} />}
+              showsHorizontalScrollIndicator={false}
+              data={options}
+              renderItem={({ item, index }) => (
+                <Button
+                  text={item.title}
+                  width={120}
+                  onPress={() => {
+                    onSelectedOption(item);
+                  }}
+                  variant={
+                    selectedOption.title === item.title ? "primary" : "outline"
+                  }
+                />
+              )}
+            />
+          </Box>
+
+          <Box mt={20}>
+            {selectedOption.title === "Informações" && (
+              <Stack spacing={10}>
                 <CardInformation
                   icon={<Icon name="payments" color="#fff" size={22} />}
-                  title={formatCurrency(downPayment) as string}
-                  description="Entrada"
-                  iconBackground="#3DE8BF"
+                  title={String(installmentsNumber)}
+                  description="Prestações"
+                  iconBackground="#000"
+                  items={getInstallmentItems(installments)}
                 />
-              )}
-              {!!installments?.at(-1)?.valuation && (
                 <CardInformation
-                  icon={<Icon name="trending-up" color="#fff" size={22} />}
-                  title={
-                    formatCurrency(installments?.at(-1)?.valuation) as string
-                  }
-                  description="Valorização"
-                  iconBackground="#3DE8BF"
-                />
-              )}
-            </Stack>
-          </Box>
-          <ReAnimated.View
-            style={{ width: "100%" }}
-            layout={Layout}
-            entering={SlideInDown.duration(1000)}
-          >
-            <Stack
-              mt={20}
-              spacing={10}
-              px={16}
-              pt={20}
-              background="tertiary"
-              radius={8}
-            >
-              {installments.map((item: any, index: number) => (
-                <CardInstallment
-                  key={String(index)}
-                  titles={["Parcela", "Valor", "Saldo devedor"]}
-                  contents={[
-                    `${String(index + 1)}º`,
-                    `${formatCurrency(item.installment)}`,
-                    `${formatCurrency(item.debit)}`,
+                  icon={<MCIcon name="percent" color="#fff" size={22} />}
+                  title={String(fee).replace(".", ",") + "%"}
+                  description="Taxa de juros anual"
+                  iconBackground="#FF3642"
+                  items={[
+                    {
+                      title: "Total juros",
+                      description: formatCurrency(
+                        installments.at(-1)?.totalFee
+                      ) as string,
+                    },
+                    {
+                      title: "Total pago",
+                      description: formatCurrency(
+                        installments.at(-1)?.installmentAmount
+                      ) as string,
+                    },
                   ]}
                 />
-              ))}
-            </Stack>
-          </ReAnimated.View>
+                {downPayment && (
+                  <CardInformation
+                    icon={<Icon name="payments" color="#fff" size={22} />}
+                    title={formatCurrency(downPayment) as string}
+                    description="Entrada"
+                    iconBackground="#3DE8BF"
+                  />
+                )}
+                {!!installments?.at(-1)?.valuation && (
+                  <CardInformation
+                    icon={<Icon name="trending-up" color="#fff" size={22} />}
+                    title={
+                      formatCurrency(installments?.at(-1)?.valuation) as string
+                    }
+                    description="Valorização"
+                    iconBackground="#3DE8BF"
+                  />
+                )}
+              </Stack>
+            )}
+
+            {selectedOption.title === "Parcelas" && (
+              <ReAnimated.View
+                style={{ width: "100%" }}
+                layout={Layout}
+                entering={SlideInDown.duration(1000)}
+              >
+                <FlatList
+                  data={simulation.installments}
+                  ItemSeparatorComponent={() => <Box height={10} />}
+                  showsVerticalScrollIndicator={false}
+                  contentContainerStyle={{ paddingBottom: 180 }}
+                  renderItem={({ item, index }) => (
+                    <CardInstallment
+                      key={String(index)}
+                      titles={["Parcela", "Valor", "Saldo devedor"]}
+                      contents={[
+                        `${String(index + 1)}º`,
+                        `${formatCurrency(item.installment)}`,
+                        `${formatCurrency(item.debit)}`,
+                      ]}
+                    />
+                  )}
+                />
+              </ReAnimated.View>
+            )}
+          </Box>
         </Box>
-      </ScrollView>
+      </Box>
     </Container>
   );
 };
